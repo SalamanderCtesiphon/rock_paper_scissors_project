6cc186007bf7feeec1e66e6a0df37c84480c4b18
SalamanderCtesiphon <swbrookshire@gmail.com>
2024-12-06 15:34:18 -0600

    made a fizzbuzz funciton

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-let name1 = "John";
+//fizzbuzz print all numbers from one to one hundred. If the number is divisible by three print fizz. If the number is divisible by five print buzz. And if the number is both divisible by three and five print fizzbuzz.
 
-console.log(name1)
+function fizzbuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+      console.log('Fizz')
+    } else if (i % 5 === 0) {
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+fizzbuzz(10000)
