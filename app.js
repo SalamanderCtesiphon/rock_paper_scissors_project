a360c5776339d378983de44e0a579a924b7f3bc9
SalamanderCtesiphon <swbrookshire@gmail.com>
2024-12-06 16:44:24 -0600

    completed the assignment of created a console based rock paper scissors game

--- app.js
+++ app.js
@@ -1,17 +1,73 @@
-//fizzbuzz print all numbers from one to one hundred. If the number is divisible by three print fizz. If the number is divisible by five print buzz. And if the number is both divisible by three and five print fizzbuzz.
+let humanScore = 0
+let computerScore = 0
 
-function fizzbuzz(n) {
-  for (let i = 1; i <= n; i++) {
-    if (i % 5 === 0 && i % 3 === 0) {
-      console.log('FizzBuzz')
-    } else if (i % 3 === 0) {
-      console.log('Fizz')
-    } else if (i % 5 === 0) {
-      console.log('Buzz')
-    } else {
-      console.log(i)
-    }
+function getComputerChoice() {
+  let computerSelections = ['Rock', 'Paper', 'Scissors']
+  let index = Math.floor(Math.random() * 3)
+  return computerSelections[index]
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt('Chose Rock, Paper or Scissors')
+  return humanChoice
+}
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`Player chooses: ${humanChoice}`)
+  console.log(`Computer chooses: ${computerChoice}`)
+  if (
+    (humanChoice === 'Rock' && computerChoice === 'Rock') ||
+    (humanChoice === 'Paper' && computerChoice === 'Paper') ||
+    (humanChoice === 'Scissors' && computerChoice === 'Scissors')
+  ) {
+    console.log('tie')
+  } else if (
+    (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
+    (humanChoice === 'Paper' && computerChoice === 'Rock') ||
+    (humanChoice === 'Scissors' && computerChoice === 'Paper')
+  ) {
+    humanScore++
+    console.log(humanScore)
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}!`)
+  } else if (
+    (humanChoice === 'Rock' && computerChoice === 'Paper') ||
+    (humanChoice === 'Paper' && computerChoice === 'Scissors') ||
+    (humanChoice === 'Scissors' && computerChoice === 'Rock')
+  ) {
+    computerScore++
+    console.log(computerScore)
+    console.log(`You Lose! ${computerChoice} beats ${humanChoice}!`)
   }
 }
 
-fizzbuzz(10000)
+function playGame() {
+  let humanChoice = getHumanChoice()
+
+  for (let i = 0; i < 5; i++) {
+    let computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+  }
+  if (humanScore > computerScore) {
+    console.log(`You win! With a score of ${humanScore} to ${computerScore}`)
+  } else if (computerScore > humanScore) {
+    console.log(
+      `Computer wins! With a score of ${computerScore} to ${humanScore}`
+    )
+  } else {
+    console.log('Game was a tie!!!!!!!!!!!!')
+  }
+}
+
+playGame()
+
+//playRound(humanChoice, computerChoice)
+
+//if (sign.toLowerCase() === 'scorpio') {
+//alert("Wow! I'm a Scorpio too!")
+//}
+
+// there are many ways to use the prompt feature
+//sign = window.prompt() // open the blank prompt window
+//sign = prompt() //  open the blank prompt window
+//sign = window.prompt('Are you feeling lucky') // open the window with Text "Are you feeling lucky"
+//sign = window.prompt('Are you feeling lucky', 'sure') // open the window with Text "Are you feeling lucky" and default value "sure"
